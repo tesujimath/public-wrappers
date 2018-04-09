d8e3668a3ca907d41eb12e5d2d72a0bc7dc1e4b5
Simon Guest <simon.guest@tesujimath.org>
2018-04-09 15:02:48 +1200

    PyPI packaging.

--- README.rst
+++ README.rst
@@ -2,15 +2,16 @@
 public-wrappers
 ===============
 
-public-wrappers provides a means to wrap programs from conda environments, so
-they may be invoked directly by users without having to explicitly activate the
-environment.  It makes use of exec-wrappers to do the wrapping.
+public-wrappers provides a means to wrap selected public programs from conda
+and/or virtualenv environments, so they may be invoked directly by users without
+having to explicitly activate the environment.  It makes use of exec-wrappers to
+do the wrapping.
 
-public-wrappers identifies only certain programs from a conda environment as
-being public, by means of a configuration file.  These public programs are
-wrapped, using exec-wrappers.
+public-wrappers identifies only certain programs from each environment as being
+public, by means of a configuration file.  These public programs are wrapped,
+using exec-wrappers.
 
-A number of conda environments may be wrapped, either into separate or a common
+A number of environments may be wrapped, either into separate or a common
 wrappers directory.
 
 Configuration
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#
+# distutils setup script for public-wrappers package
+
+from setuptools import setup, find_packages
+
+long_description = """public-wrappers provides a means to wrap selected public programs from conda
+environments, so they may be invoked directly by users without having to
+explicitly activate the environment.  It makes use of exec-wrappers to do the
+wrapping.
+"""
+
+setup(name='public-wrappers',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
+      description='Wrap selected public programs from conda/virtualenv environments',
+      long_description=long_description,
+      author='Simon Guest',
+      author_email='simon.guest@tesujimath.org',
+      url='https://github.com/tesujimath/public-wrappers',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: System :: Systems Administration',
+          'Topic :: Utilities',
+      ],
+      packages=find_packages(),
+      entry_points={
+        'console_scripts': [
+            'configure-public-wrappers = public_wrappers.__main__:main',
+        ],
+      },
+      package_data={
+          'doc': ['*.rst'],
+      },
+      license='GPLv3',
+      install_requires=[
+          'future',
+          'pytoml',
+      ],
+      python_requires='>=2.7',
+     )
